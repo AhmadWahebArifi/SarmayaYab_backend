9a80abc547bfa4edde54be5063b3534702bb678a
Ahmad Arifi <ahmadwahebarifi@gmail.com>
2025-12-22 10:28:15 +0430

    feat(config): add broadcasting.php configuration file
    - Create new broadcasting.php configuration file
    - Define default broadcast driver with environment variable fallback
    - Add connection settings for pusher,

---
+++ config/broadcasting.php
@@ -0,0 +1,51 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Broadcasting Connections
+    |--------------------------------------------------------------------------
+    |
+    | Below you will find a configuration array for all broadcasting
+    | connections. These connections are used to broadcast events to multiple
+    | systems or over different types of transports.
+    |
+    */
+
+    'default' => env('BROADCAST_DRIVER', 'null'),
+
+    'connections' => [
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+            ],
+        ],
+
+        'ably' => [
+            'driver' => 'ably',
+            'key' => env('ABLY_KEY'),
+        ],
+
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default',
+        ],
+
+        'log' => [
+            'driver' => 'log',
+        ],
+
+        'null' => [
+            'driver' => 'null',
+        ],
+
+    ],
+
+];
