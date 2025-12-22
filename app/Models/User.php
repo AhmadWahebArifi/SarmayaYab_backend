ba32db22a8770e8e6cdeb4547b14e66750cf7b64
Ahmad Arifi <ahmadwahebarifi@gmail.com>
2025-12-22 10:27:37 +0430

    feat(user): add User model with relationships and attribute casting
    - Create User model class extending Authenticatable
    - Include traits for API tokens, factory, and notifications
    - Define mass assignable attributes: name, email, password, phone, balance, status
    - Hide password and remember_token in serialization
    - Cast email_verified_at as datetime, password as hashed, and balance as decimal with 2 digits
    - Add one-to-many relationship methods to investments and transactions models

---
+++ app/Models/User.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone',
+        'balance',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'balance' => 'decimal:2',
+    ];
+
+    /**
+     * Get the investments for the user.
+     */
+    public function investments()
+    {
+        return $this->hasMany(Investment::class);
+    }
+
+    /**
+     * Get the transactions for the user.
+     */
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
