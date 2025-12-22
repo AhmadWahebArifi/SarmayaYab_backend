2a552df52b9a5a152f3ad078a78d40bbac818bca
Ahmad Arifi <ahmadwahebarifi@gmail.com>
2025-12-22 10:26:51 +0430

    chore(middleware): add TrustProxies middleware class
    - Create TrustProxies class extending base middleware
    - Define $proxies property as null to trust all proxies by default
    - Set $headers property to 0 for header detection configuration
    - Add PHPDoc comments for class properties and description

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    protected $proxies = null;
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = 0;
+}
