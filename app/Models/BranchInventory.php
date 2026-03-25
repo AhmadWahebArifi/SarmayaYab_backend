271ec5ab6ca1ed70dd36115b02a9ceb574081bea
Ahmad Arifi <ahmadwahebarifi@gmail.com>
2026-03-25 14:18:04 +0430

    Branch inventory created

---
+++ app/Models/BranchInventory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BranchInventory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'branch_inventories';
+
+    protected $fillable = [
+        'branch_id',
+        'product_id',
+        'quantity',
+    ];
+
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
