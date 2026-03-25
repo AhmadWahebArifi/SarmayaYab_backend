e787b48900d75cc764a60298c32627394d1fc630
Ahmad Arifi <ahmadwahebarifi@gmail.com>
2026-03-25 14:19:14 +0430

    Store branch Request for handling !

---
+++ app/Http/Requests/Branch/StoreBranchRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Branch;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBranchRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'code' => ['required', 'string', 'max:50', 'unique:branches,code'],
+            'region' => ['nullable', 'string', 'max:255'],
+            'city' => ['nullable', 'string', 'max:255'],
+            'address' => ['nullable', 'string', 'max:255'],
+            'status' => ['sometimes', 'string', 'in:active,inactive'],
+        ];
+    }
+}
