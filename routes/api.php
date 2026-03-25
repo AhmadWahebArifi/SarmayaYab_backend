8de4e4ab4a0cd08a71f93b8aec450b4d6ce23ec2
Ahmad Arifi <ahmadwahebarifi@gmail.com>
2026-03-25 14:39:54 +0430

    API

--- routes/api.php
+++ routes/api.php
@@ -6,29 +6,19 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\InvestmentController;
 use App\Http\Controllers\TransactionController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\StockRequestController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
-    // User routes
     Route::get('/user', [UserController::class, 'show']);
     Route::put('/user', [UserController::class, 'update']);
     Route::get('/user/balance', [UserController::class, 'balance']);
 
-    // Investment routes
     Route::get('/investments', [InvestmentController::class, 'index']);
     Route::post('/investments', [InvestmentController::class, 'store']);
     Route::get('/investments/{investment}', [InvestmentController::class, 'show']);
@@ -36,9 +26,30 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/investments/{investment}', [InvestmentController::class, 'destroy']);
     Route::get('/investments/summary', [InvestmentController::class, 'summary']);
 
-    // Transaction routes
     Route::get('/transactions', [TransactionController::class, 'index']);
     Route::post('/transactions', [TransactionController::class, 'store']);
     Route::get('/transactions/{transaction}', [TransactionController::class, 'show']);
     Route::get('/transactions/summary', [TransactionController::class, 'summary']);
+
+    // Inventory system routes
+    Route::middleware('role:admin,warehouse_staff')->group(function () {
+        Route::apiResource('products', ProductController::class);
+        Route::apiResource('branches', BranchController::class);
+    });
+
+    Route::middleware('role:admin,warehouse_staff,branch_manager,branch_staff')->group(function () {
+        Route::get('/stock-requests', [StockRequestController::class, 'index']);
+        Route::post('/stock-requests', [StockRequestController::class, 'store']);
+        Route::get('/stock-requests/{stockRequest}', [StockRequestController::class, 'show']);
+    });
+
+    Route::middleware('role:admin,warehouse_staff')->group(function () {
+        Route::post('/stock-requests/{stockRequest}/approve', [StockRequestController::class, 'approve']);
+        Route::post('/stock-requests/{stockRequest}/reject', [StockRequestController::class, 'reject']);
+        Route::post('/stock-requests/{stockRequest}/dispatch', [StockRequestController::class, 'dispatch']);
+    });
+
+    Route::middleware('role:admin,branch_manager,branch_staff')->group(function () {
+        Route::post('/stock-requests/{stockRequest}/deliver', [StockRequestController::class, 'deliver']);
+    });
 });
